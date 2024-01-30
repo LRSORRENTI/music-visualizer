1523cc43e0bd7b32666af259da3e9633db411f9f
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-01-30 16:54:55 -0600

    file select is now correctly using user selected audio files

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ container.addEventListener('click', () => {
     let audio1 = new Audio();
     const audioContext = new (window.AudioContext)();
     console.log(audioContext)
-    audio1.src = 'audio/Narvent-Memory-Reboot.mp3'
+    // audio1.src = 'audio/Narvent-Memory-Reboot.mp3'
     audio1.play();
     audioSource = audioContext.createMediaElementSource(audio1);
     analyser = audioContext.createAnalyser();
@@ -54,7 +54,43 @@ container.addEventListener('click', () => {
     animate()
 });
 
-file.addEventListener('change', () => {
-    // console.log(this.files)
+file.addEventListener('change', function() {
+    // console.log(this.files
     const files = this.files;
+    const audio1 = document.getElementById('audio1');
+    // const audioContext = new (window.AudioContext)();
+    audio1.src = URL.createObjectURL(files[0]);
+    audio1.load();
+    audio1.play();
+
+    audioSource = audioContext.createMediaElementSource(audio1);
+    analyser = audioContext.createAnalyser();
+    audioSource.connect(analyser);
+    analyser.connect(audioContext.destination);
+    analyser.fftSize = 64;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    const barWidth = canvas.width / bufferLength;
+    let barHeight;
+    let x;
+
+    function animate(){
+        x = 0;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        analyser.getByteFrequencyData(dataArray);
+        for(let i = 0; i < bufferLength; i++) {
+            // this will take the bar height and set it 
+            // equal to the dataArray of i, louder sounds 
+            // produce larger bars
+            barHeight = dataArray[i] * 2;
+            ctx.fillStyle = 'lime';
+            ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight)
+            // the x+= below will place the bars next to 
+            // one another along the x-axis
+            x += barWidth;
+        }
+        requestAnimationFrame(animate);
+    }
+    animate()
 })
