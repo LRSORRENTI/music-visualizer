8dbbb79f309774a6d4e65ae0afca2b1cf550b634
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-01-30 15:19:58 -0600

    Added audio file and  aplay button to start audio

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const button1 = document.getElementById('button1');
+
+let audio1 = new Audio();
+
+audio1.src = 'audio/Narvent-Memory-Reboot.mp3'
+
 button1.addEventListener('click', () => {
-    console.log("Clicked")
+    audio1.play();
+    audio1.addEventListener('playing', () => {
+        console.log('audio 1 has started playing')
+    });
+    audio1.addEventListener('ended', () => {
+        console.log('audio has ended')
+    })
 })
