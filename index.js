00634793a0782f201feb74470ed233b078b02484
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-01-30 15:30:53 -0600

    added audioContext instantiation for web audio api

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const button1 = document.getElementById('button1');
 
 let audio1 = new Audio();
+const audioCtx = new (window.AudioContext)();
+console.log(audioCtx)
 
 audio1.src = 'audio/Narvent-Memory-Reboot.mp3'
 
@@ -13,3 +15,11 @@ button1.addEventListener('click', () => {
         console.log('audio has ended')
     })
 })
+
+function playSound(){
+    audio1.play();
+}
+
+const button2 = document.getElementById('button2');
+
+button2.addEventListener('click', playSound)
