a46ceaafe6f12d0d7533fce862b3d842e5c0ae5a
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-01-30 16:24:17 -0600

    audio bars are appearing on the canvas element now

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-let audio1 = new Audio();
-const audioContext = new (window.AudioContext)();
-console.log(audioContext)
-audio1.src = 'audio/Narvent-Memory-Reboot.mp3'
+// let audio1 = new Audio();
+// const audioContext = new (window.AudioContext)();
+// console.log(audioContext)
+// audio1.src = 'audio/Narvent-Memory-Reboot.mp3'
 
 const container = document.getElementById('container');
 
@@ -15,6 +15,10 @@ let audioSource;
 let analyser;
 
 container.addEventListener('click', () => {
+    let audio1 = new Audio();
+    const audioContext = new (window.AudioContext)();
+    console.log(audioContext)
+    audio1.src = 'audio/Narvent-Memory-Reboot.mp3'
     audio1.play();
     audioSource = audioContext.createMediaElementSource(audio1);
     analyser = audioContext.createAnalyser();
@@ -22,4 +26,28 @@ container.addEventListener('click', () => {
     analyser.connect(audioContext.destination);
     analyser.fftSize = 64;
     const bufferLength = analyser.frequencyBinCount;
-})
+    const dataArray = new Uint8Array(bufferLength);
+
+    const barWidth = canvas.width / bufferLength;
+    let barHeight;
+    let x;
+
+    function animate(){
+        x = 0;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        analyser.getByteFrequencyData(dataArray);
+        for(let i = 0; i < bufferLength; i++) {
+            // this will take the bar height and set it 
+            // equal to the dataArray of i, louder sounds 
+            // produce larger bars
+            barHeight = dataArray[i];
+            ctx.fillStyle = 'lime';
+            ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight)
+            // the x+= below will place the bars next to 
+            // one another along the x-axis
+            x += barWidth;
+        }
+        requestAnimationFrame(animate);
+    }
+    animate()
+});
