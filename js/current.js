35d23959503b48aac14e9f074f4bb62fb254d1c2
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-02-03 16:21:29 -0600

    still messing around with the visualizer, may take away some of the audio control options

--- js/current.js
+++ js/current.js
@@ -45,6 +45,14 @@ container.addEventListener('click', () => {
     }
     animate()
 });
+// colors for visualizer
+const colors = [
+    'hsl(72, 100%, 50%)',  // #CFFF04
+    'hsl(182, 100%, 42%)', // #00D1DC
+    'hsl(59, 100%, 50%)',
+    'hsl(0, 11%, 100%)'
+];
+
 
 function drawVisualizer(bufferLength, x, barWidth, barHeight, dataArray){
     // for(let i = 0; i < bufferLength; i++) {
@@ -64,29 +72,70 @@ function drawVisualizer(bufferLength, x, barWidth, barHeight, dataArray){
     //     // one another along the x-axis
     //     x += barWidth;
     // }
-    for(let i = 0; i < bufferLength; i++) {
+    // for(let i = 0; i < bufferLength; i++) {
+    //     barHeight = dataArray[i] * 1.5;
+    //     ctx.save();
+    //     ctx.translate(canvas.width / 2, canvas.height / 2)
+    //     ctx.rotate(i * Math.PI * 20 / bufferLength);
+    //     const hue = i * 0.3;
+    //     ctx.fillStyle = 'hsl(' + hue + ' ,100%,' + barHeight / 3 + '%)'
+    //     ctx.fillRect(0, 0, barWidth, barHeight)
+    //     x += barWidth;
+    //     ctx.restore();
+    // }
+    // for(let i = 10; i < bufferLength; i++) {
+    //     barHeight = dataArray[i] * 1.5;
+    //     ctx.save();
+    //     ctx.translate(canvas.width / 2, canvas.height / 2)
+    //     ctx.rotate(i * Math.PI * 100 / bufferLength);
+    //     const hue = i * 0.3;
+    //     ctx.fillStyle = 'hsl(' + hue + ' ,100%,' + barHeight / 3 + '%)'
+    //     ctx.fillRect(0, 0, barWidth, barHeight)
+    //     x += barWidth;
+    //     ctx.restore();
+    // }
+    for (let i = 0; i < bufferLength; i++) {
         barHeight = dataArray[i] * 1.5;
+        const color = colors[i % colors.length]; // Cycle through the color array
         ctx.save();
-        ctx.translate(canvas.width / 2, canvas.height / 2)
+        ctx.translate(canvas.width / 2, canvas.height / 2);
         ctx.rotate(i * Math.PI * 20 / bufferLength);
-        const hue = i * 0.3;
-        ctx.fillStyle = 'hsl(' + hue + ' ,100%,' + barHeight / 3 + '%)'
-        ctx.fillRect(0, 0, barWidth, barHeight)
-        x += barWidth;
-        ctx.restore();
-    }
-    for(let i = 10; i < bufferLength; i++) {
-        barHeight = dataArray[i] * 1.5;
-        ctx.save();
-        ctx.translate(canvas.width / 2, canvas.height / 2)
-        ctx.rotate(i * Math.PI * 100 / bufferLength);
-        const hue = i * 0.3;
-        ctx.fillStyle = 'hsl(' + hue + ' ,100%,' + barHeight / 3 + '%)'
-        ctx.fillRect(0, 0, barWidth, barHeight)
+        ctx.fillStyle = color;  // Use the color from the array
+        ctx.fillRect(0, 0, barWidth, barHeight);
         x += barWidth;
         ctx.restore();
     }
 }
+
+// Function to initialize the visualizer
+function initializeVisualizer(audioElement) {
+    const audioContext = new (window.AudioContext)();
+    const audioSource = audioContext.createMediaElementSource(audioElement);
+    const analyser = audioContext.createAnalyser();
+    audioSource.connect(analyser);
+    analyser.connect(audioContext.destination);
+    analyser.fftSize = 1024; // or 2048 as in your file upload handling
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    const barWidth = 15;
+    let barHeight;
+    let x;
+
+    function animate() {
+        x = 0;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        analyser.getByteFrequencyData(dataArray);
+        drawVisualizer(bufferLength, x, barWidth, barHeight, dataArray);
+        requestAnimationFrame(animate);
+    }
+    animate();
+}
+
+// Get the default audio element and set up the visualization on play event
+const defaultAudio = document.getElementById('audio1');
+defaultAudio.addEventListener('play', function() {
+    initializeVisualizer(this);
+});
 
 file.addEventListener('change', function() {
     // console.log(this.files
@@ -96,7 +145,7 @@ file.addEventListener('change', function() {
     audio1.src = URL.createObjectURL(files[0]);
     audio1.load();
     audio1.play();
-
+    initializeVisualizer(audio1);
     audioSource = audioContext.createMediaElementSource(audio1);
     analyser = audioContext.createAnalyser();
     audioSource.connect(analyser);
